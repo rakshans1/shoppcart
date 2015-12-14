f0c4de3dc9eaa220541a6ad1beb3deb01bb7b10f
Rakshan Shetty <shetty.raxx555@gmail.com>
2015-12-14 16:01:37 +0530

    user data added

--- core/functions/users.php
+++ core/functions/users.php
@@ -1,4 +1,27 @@
 <?php 
+function user_data($user_id){
+	$data = array();
+	$user_id = (int)$user_id;
+
+	$func_num_args = func_num_args();
+	$func_get_args = func_get_args();
+
+	if ($func_get_args > 1) {
+		unset($func_get_args[0]);
+
+		$fields = '`'. implode('`, `', $func_get_args). '`';
+
+		$data = mysql_fetch_assoc(mysql_query("SELECT $fields FROM `users` WHERE `user_id` = $user_id" ));
+		return $data;
+
+	}
+
+
+
+}
+
+
+ 
 function logged_in(){
 	return (isset($_SESSION['user_id']))? true : false;
 }
@@ -31,5 +54,9 @@ function login($username, $password) {
 	return (mysql_result(mysql_query("SELECT COUNT(`user_id`) FROM `users` WHERE `username`= '$username' AND `password` = '$password' "), 0) == 1) ? $user_id : false;
 
 }
+
+
+
+
 
 ?>
--- core/init.php
+++ core/init.php
@@ -8,6 +8,14 @@ require 'database/connect.php';
 require 'functions/general.php';
 require 'functions/users.php';
 
+if (logged_in() === true ){
+	
+	$session_user_id = $_SESSION['user_id'];
+	$user_data = user_data($session_user_id, 'user_id','username','password','first_name','last_name','email');
+}
+
+
+
 $errors = array();
 
 
