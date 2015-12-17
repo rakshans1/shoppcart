0aecb6f2b627ba1d009b71ca7aadb27f99b2754b
Rakshan Shetty <shetty.raxx555@gmail.com>
2015-12-17 23:18:16 +0530

    Feature $ trend Random

--- html/php/includes/itrend.php
+++ html/php/includes/itrend.php
@@ -1,209 +1,43 @@
 <div class="container">
 <div class="recommended_items"><!--recommend item -->
                             <h2 class="title text-center">Trending</h2>
-
-
 <div class="carousel slide">
-
 <div id="owl-demo" class="owl-carousel owl-item ">
-  <div class="item">
-                  <div class="col-sm-12 col-md-12">
-                <div class="product-image-wrapper">
-                    <div class="single-products">
-                        <div class="productinfo text-center">
-                            <img src="images/home/recommend1.jpg" alt="" />
-                            <h2>Rs.56</h2>
-                            <p>Nyle Shampoo Strong & Smooth 800 ML</p>
-                            <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-                        </div>
-                        
-                    </div>
-                </div>
-            </div>
-
-  </div>
-  <div class="item">
-      <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend2.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Kesh King Apple Aloevera Conditioner 115 ML</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-</div>
-<div class="item">
-                  <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend7.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Pantene Shampoo Silky Smooth Care 180 ML Free Conditioner</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-</div>
-<div class="item">
-                  <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend3.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Pantene Shampoo Long Black 80 ML With Free Olay Face Wash</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-    </div>
-
-    <div class="item">
-                  <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend4.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Cerelac Stage 1 Apple + Wheat 375 GM</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-    </div>
-
-<div class="item">
-                  <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend5.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Pantene Shampoo Silky Smooth Care 180 ML Free Conditioner</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-    </div>
-    <div class="item">
-                  <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend6.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Pantene Shampoo Silky Smooth Care 180 ML Free Conditioner</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-    </div>
-
-<div class="item">
-                  <div class="col-sm-12 col-md-12">
-    <div class="product-image-wrapper">
-        <div class="single-products">
-            <div class="productinfo text-center">
-                <img src="images/home/recommend9.jpg" alt="" />
-                <h2>Rs.56</h2>
-                <p>Pantene Shampoo Silky Smooth Care 180 ML Free Conditioner</p>
-                <div class="row">
-                        <div class="col-sm-6">
-                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
-                        </div>
-                        <div class="col-sm-6">
-                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
-                        </div>
-                    </div> 
-            </div>
-            
-        </div>
-    </div>
-</div>
-    </div>
+<?php
 
 
- 
-
+  $conn = mysqli_connect("localhost","root","","shoppcart") ;
+   
+   if(! $conn )
+   {
+      die('Could not connect: ' . mysql_error());
+   }
+   $sql = 'SELECT * FROM `cat` ORDER BY     RAND() LIMIT 0,6;';
+   $result = $conn->query($sql);
+ if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+      
+    $pname=$row["pname"];
+    $pprice=$row["pprice"];  
+    $pimage=$row["pimage"];  
+    $pdesc=$row["pdesc"];  
+    $pquant=$row["pquant"];       
+    include  "trend.php";
+          }
+   } else {
+    echo "0 results";
+}   
+   mysqli_close($conn);
+?>
 </div>
- 
-
-
 <div class="customNavigation hidden-xs">
-                <a class="btn prev right recommended-item-control ">
+                <a class="btn  next right recommended-item-control ">
                 <i class="fa fa-angle-right"></i>
                 </a>
-                <a class="btn next left recommended-item-control "  data-slide="prev">
+                <a class="btn  prev left  recommended-item-control ">
                 <i class="fa fa-angle-left"></i>
                 </a>
 </div>
-
-
 </div>
 </div><!--recommend item end-->
 </div>
---
+++ html/php/includes/trend.php
@@ -0,0 +1,33 @@
+  <div class="item">
+                  <div class="col-sm-12 col-md-12">
+    <div class="product-image-wrapper">
+        <div class="single-products">
+                <div class="productinfos text-center">
+                    <img class ="img-thumbnail " src="<?php echo $pimage; ?>"  alt="" />
+                    <h2>Rs.<?php echo $pprice; ?></h2>
+                    <?php $srt = str_replace("_"," ","$pname");?>
+                    <p><?php echo strtoupper ($srt); ?></p>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                        <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-shopping-cart"></i>Add to cart</a>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="#" class="btn btn-default btn-block add-to-cart"><i class="fa fa-info-circle"></i>Details</a>
+                        </div>
+                    </div>    
+                </div>
+        </div>
+        <div class="choose">
+            <ul class="nav nav-pills nav-justified">
+                <li><a href="#"><i class="fa fa-plus-square"></i>Add to wishlist</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+  </div>
+
+ 
+
+
