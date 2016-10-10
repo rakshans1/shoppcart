b745f752e1a6644f4f01ebf827ddb3a379dd0128
Rakshan <shetty.raxx555@gmail.com>
2016-10-10 20:55:00 +0530

    Composer Added

--- core/database/connect.php
+++ core/database/connect.php
@@ -6,8 +6,8 @@ $username = $url["user"];
 $password = $url["pass"];
 $db = substr($url["path"], 1);
 
-mysql_connect('localhost','root','123');
+mysql_connect($server,$username, $password);
 mysql_select_db($db);
-// $connection = mysqli_connect('localhost','root','123','shoppcart');
+$connection = mysqli_connect('localhost','root','123','shoppcart');
 
 ?>
