dec9b45de080d3ff396ee24316c0365f90f8b958
Rakshan <shetty.raxx555@gmail.com>
2016-10-10 20:09:51 +0530

    DB added

--- core/database/connect.php
+++ core/database/connect.php
@@ -1,6 +1,13 @@
 <?php
-mysql_connect('localhost','root','123');
-mysql_select_db('shoppcart');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+// mysql_connect('localhost','root','123');
+// mysql_select_db('shoppcart');
 $connection = mysqli_connect('localhost','root','123','shoppcart');
 
 ?>
